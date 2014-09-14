37f6397a7f5f34574daff30846eb844534d5d199
amni <amni2015@gmail.com>
2014-09-14 00:57:12 -0400

    change messages

--- js/transitions.js
+++ js/transitions.js
@@ -1,8 +1,7 @@
 function changeBg(img_name) {
+    $('.intro-header').fadeOut(1000);  
+    $('.intro-header').fadeIn(1000);  
     $('.intro-header').css('backgroundImage', function () {
-      $('.intro-header').delay(3000).animate({
-          backgroundColor: 'rgb(255,255,255)'
-      }, 3000);
         return "url('img/" + img_name + ".jpg')";
     });
 };
@@ -11,10 +10,10 @@ function addTextBubble(text_input, is_user) {
   if (is_user){
     $('.text').hide(); 
     setTimeout(function () {
-    $('.user-text').text(text_input);
+    $('.user-text').html(text_input);
     $('.user-text').show();}, 200); 
   } else {
-    $('.smsmart-text').text(text_input);
+    $('.smsmart-text').html(text_input);
     $('.smsmart-text').show(); 
   }
 };
@@ -24,10 +23,9 @@ function addTextBubble(text_input, is_user) {
     var interval = 5000, 
         index = 0,
         is_user = true, 
-        img_arr = [{user_text_image: 'directions', response_text_image: 'map'}, {user_text_image: 'directions', response_text_image: 'map'}, {user_text_image: 'directions', response_text_image: 'map'}],
-        text_message_arr = [{user_text: "maps: directions from Duke Chapel to Wannamaker Drive", response_text: '1. Turn Left\n2. Then go right\n3. You arrived at the destination'}, {user_text: "maps: directions from Duke Chapel to Wannamaker Drive", response_text: '1. Turn Left\n2. Then go right\n3. You arrived at the destination'}, {user_text: "maps: directions from Duke Chapel to Wannamaker Drive", response_text: '1. Turn Left\n2. Then go right\n3. You arrived at the destination'}];
+        img_arr = [{user_text_image: 'map', response_text_image: 'directions'}, {user_text_image: 'receipts', response_text_image: 'restaurant'}, {user_text_image: 'cab', response_text_image: 'goldengate'}],
+        text_message_arr = [{user_text: "maps: directions from E Evelyn Ave to Castro Street", response_text: '1. Head west on E Evelyn Ave toward Ferry Morse Way after 0.6 miles <br> 2. Turn left onto Calderon Ave after 0.5 miles <br> 3. Turn right onto Church St after 0.4 miles'}, {user_text: "yelp: find coffee shops on castro street", response_text: 'Red Rock Coffee <br> 3.5 Stars <br> 201 Castro Street'}, {user_text: "venmo: pay Ben 60.00 for taxi to Golden Gate Bridge", response_text: 'You just paid Ben 60.00 for a taxi to Golden Gate Bridge'}];
     function makeTransition() {
-      changeBg();  
       img_object = img_arr[index % text_message_arr.length]; 
       text_message_object = text_message_arr[index % text_message_arr.length]; 
 
@@ -37,9 +35,9 @@ function addTextBubble(text_input, is_user) {
       } else {
         changeBg(img_object.response_text_image); 
         setTimeout('addTextBubble(text_message_object.response_text, false)', 500);
+        index+=1; 
       }
       is_user = !is_user 
-      index+=1; 
     }
     makeTransition(); 
     setInterval(makeTransition, interval); 
