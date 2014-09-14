0da49e07fded9134be6a939500a34ff1ae3bd12c
= <benjamin.schwab@duke.edu>
2014-09-14 02:58:02 -0400

    actually fixed enter to sign up

--- js/signup.js
+++ js/signup.js
@@ -2,21 +2,23 @@
    var emailBox = document.getElementById("emailForm");
    var submitButton = document.getElementById("submitEmail");
    submitButton.addEventListener("click", function(){
-      console.log(emailBox.value);
-      if(signup(emailBox.value)){
+      sendsignup();
+   });
+
+   function sendsignup(){
+       if(signup(emailBox.value)){
          $('.signup-container').fadeOut(400);
          $('.thank-you-message').fadeIn(400);
       }
       else{
          console.log("not valid email");
       }
-
-   });
+   }
 
    document.addEventListener("keypress", function(e){
       var key = e.which || e.keyCode;
        if (key == 13) { // 13 is enter
-         signup(emailBox.value);
+         sendsignup();
        }
    });
 
