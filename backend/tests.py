34faefca4e19f868f36969673e2ff877cc0a56c9
Jay Wang <jmw86@duke.edu>
2014-11-26 01:01:49 -0600

    Added key to commands so tests pass. Key is now a required feature in every command.

--- backend/tests.py
+++ backend/tests.py
@@ -1,4 +1,3 @@
-
 import app
 import unittest
 from models import User, Variable
@@ -10,20 +9,21 @@ class TestParser(unittest.TestCase):
         new_user = User(phone_number="5734894023")
         new_user.save()
 
-    def test_process_message(self):
-        """Checks that that modules are loading correctly"""
+    """def test_yelp_splitting(self):
         default_user = User.objects(phone_number="5734894023").first()
-        print app.process_message(default_user, "@Yelp search:  longlat: true near: 40.74503998,-73.99879607 category: Pizza")
-        print app.process_message(default_user, "attractions help")
-        print app.process_message(default_user, "attractions search: near: paris")
-        print app.process_message(default_user, "@Yelp: near: Durham, NC category: bars")
-        print app.process_message(default_user, "yelp help")
-        print app.process_message(default_user, "maps help")
-        print app.process_message(default_user, "@yelp help")
-        print app.process_message(default_user, "Yelp: near: Durham, NC category: bars")
-        print app.process_message(default_user, "yelp info: place: 1")
-        print app.process_message(default_user, "Yelp search: near: chapel hill, NC distance: 5")
-        print app.process_message(default_user, "maps directions: from: chapel hill, NC to: Durham, NC")
+        output = app.process_message(default_user, "@ Yelp search:  longlat: true near: 36.0053098,-78.9260807 category: Food key: l")
+        app.distribute("4086934876", output)"""
+
+    def test_process_message(self):
+        default_user = User.objects(phone_number="5734894023").first()
+        print app.process_message(default_user, "@ Wikipedia search: limit: summary: Pinterest sentences: 3 key: z")
+        print app.process_message(default_user, "@ Wikipedia: search: cars limit: 5 key: z")
+        print app.process_message(default_user, "@Yelp search:  longlat: true near: 40.74503998,-73.99879607 category: Pizza key: z")
+        print app.process_message(default_user, "@Yelp: near: Durham, NC category: bars key: z")
+        print app.process_message(default_user, "Yelp: near: Durham, NC category: bars key: z")
+        print app.process_message(default_user, "yelp info: place: 1 key: z")
+        print app.process_message(default_user, "Yelp search: near: chapel hill, NC distance: 5 key: z")
+        print app.process_message(default_user, "maps directions: from: chapel hill, NC to: Durham, NC key: z")
 
     def test_parse_module(self):
         """Checks that the parser parses the first word correctly"""
