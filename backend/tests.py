bc8aaade329a46b7cbd54ebe9fdf1e59ddde4d26
Jay Wang <jmw86@duke.edu>
2014-12-16 21:00:16 -0800

    New distributed testing class in tests.

--- backend/tests.py
+++ backend/tests.py
@@ -3,12 +3,23 @@ import unittest
 from models import User, Query
 
 
+
+class TestDistribute(unittest.TestCase):
+    def setup(self):
+        self.app = app.test_client(use_cookies=True)
+        new_user = User(phone_number="5734894023")
+        new_user.save()    
+
+    def test_distribute(self):
+        app.distribute("+14086934876", "The Ford Motor Company (colloquially referred to as Ford) is an American multinational automaker headquartered in Dearborn, Michigan, a suburb of Detroit. It was founded by Henry Ford and incorporated on June 16, 1903. The company sells automobiles and commercial vehicles under the Ford brand and most luxury cars under the Lincoln brand. Ford also owns Brazilian SUV manufacturer, Troller, and Australian performance car manufacturer FPV.")
+
+
 class TestParser(unittest.TestCase):
     def setup(self):
         self.app = app.test_client(use_cookies=True)
         new_user = User(phone_number="5734894023")
         new_user.save()
-
+    
     def test_process_message(self):
         default_user = User.objects(phone_number="5734894023").first()
         print 'Query: @ Wikipedia search: term: Ford limit: 3 key: z'
@@ -18,6 +29,8 @@ class TestParser(unittest.TestCase):
         print 'Query: @ Wikipedia summary: term: cars limit: 5 key: z'
         print app.process_message(default_user, "@ Wikipedia summary: term: cars limit: 5 key: z")
         print 
+        print
+        print
         print
         print 'Query: @Yelp search: limit: 1 longlat: true near: 40.74503998,-73.99879607 category: Pizza key: z' 
         print app.process_message(default_user, "@Yelp search:  longlat: true near: 40.74503998,-73.99879607 category: Pizza key: z limit: 1")
@@ -44,5 +57,7 @@ class TestParser(unittest.TestCase):
         """Checks that the parser parses handles distributed texting properly"""
         pass
     
+distribute = TestDistribute
+
 if __name__ == "__main__":
     unittest.main()
