cf00b3ac65020f266daa0fc9bda98a1b642dc8a1
Jay Wang <jmw86@duke.edu>
2014-12-28 20:21:53 -0800

    added case when API returns None for an invalid category

--- backend/controller/api/wrapper/yelp_wrapper.py
+++ backend/controller/api/wrapper/yelp_wrapper.py
@@ -7,6 +7,8 @@ def query(location, distance = 5.0, category='restaurant', limit = 6):
         result = yelp.query_api(category, location, distance, False, 1, limit)
     except:
         result = '1'
+    if result == None: #sometimes result will return None for an invalid category 
+        result = '1'
     return result
 
 def query_geo(lat, lng, distance = 5.0, category='restaurant', limit = 6):
@@ -15,5 +17,7 @@ def query_geo(lat, lng, distance = 5.0, category='restaurant', limit = 6):
         result = yelp.query_api_geo(category, geo, distance, False, 1, limit)
     except:      
         result = '1'
+    if result == None:
+        result = '1'        
     return result
 
