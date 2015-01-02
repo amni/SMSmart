e91a276636096e168071b681dab2aab918b5e22e
amni <amni2015@gmail.com>
2015-01-02 02:00:27 -0500

    change list to dict

--- backend/app.py
+++ backend/app.py
@@ -20,7 +20,7 @@ account_sid = "AC171ca34ca45bf15bb3f369b1ae5e9a9f"
 auth_token = "1d3ef112c1407035c6c6f5e5e17f75ad"
 client = TwilioRestClient(account_sid, auth_token)
 PHONE_NUMBERS = ["+15738182146", "+19738280148", "+16503534855", "+18704740576", "+18702802312"]
-PLANS = ["Budget":50, "Pro": 100, "Premium":200]
+PLANS = {"Free": 30, "Budget":50, "Pro": 100, "Premium":200, "Unlimited": 10000}
 MSG_SEGMENT_LENGTH = 150
 #for heroku
 if 'PORT' in os.environ: 
